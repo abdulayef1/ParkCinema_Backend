b782601617f719beca6d22a4ba9ec2cccbd43cb8
abdulayef1 <7ka1cy@.code.edu.az>
2023-02-06 12:41:47 +0400

    initial

---
+++ ParkCinema/src/ParkCinema.DataAccess/Contexts/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ParkCinema.DataAccess.Contexts;
+
+public class AppDbContext:DbContext
+{
+	public AppDbContext(DbContextOptions<AppDbContext> options):base(options)	
+	{
+
+	}
+}
